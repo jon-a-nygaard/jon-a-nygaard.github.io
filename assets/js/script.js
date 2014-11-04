bf7bffd45670dc3c8d4451a38d1623a96a5053dc
jon-a-nygaard <jon.arild@highsoft.com>
2014-11-04 18:53:24 +0100

    Update of layout and content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-// jQuery(document).ready(function() {
-// 	setPageHeight();
-// 	jQuery(window).on('resize', function() {
-// 		setPageHeight();
-// 	});
+jQuery(document).ready(function() {
+	setPageHeight();
+	jQuery(window).on('resize', function() {
+		setPageHeight();
+	});
 
-// 	// Add scrollanimation to button
-// 	jQuery('a[href*=#]:not([href=#])').click(function() {
-// 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-// 			var target = jQuery(this.hash);
-// 			target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
-// 			if (target.length) {
-// 				jQuery('html,body').animate({
-// 				scrollTop: target.offset().top
-// 				}, 1000);
-// 				return false;
-// 			}
-// 		}
-// 	});
-// });
-// function setPageHeight() {
-// 	height = jQuery(window).height();
-// 	jQuery(".page").css("height", height);
-// }
+	// Add scrollanimation to button
+	jQuery('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = jQuery(this.hash);
+			target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				jQuery('html,body').animate({
+				scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+function setPageHeight() {
+	height = jQuery(window).height();
+	jQuery(".page").css("height", height);
+}
